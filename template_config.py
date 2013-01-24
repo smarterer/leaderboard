fb027ef70e42255d92deedd634ac06484562f9c2
mikepk <mikepk@smarterer.com>
2013-01-24 18:55:20 -0500

    Remove test values from config template, fix typo

--- template_config.py
+++ template_config.py
@@ -1,13 +1,13 @@
 
-SMARTERER_CLIENT_ID = "bcbd4d3dbf744576857b2c2970a66f69"
-SMARTERER_APP_SECRET = "60855b531dc7cf9c68e2f6f86ab92248"
+SMARTERER_CLIENT_ID = "XXXX"
+SMARTERER_APP_SECRET = "XXXX"
 
 SMARTERER_WIDGET_EMBED = '''
-<script type="text/javascript" src="http://smarterer.com/js/popup.js"></script><a href="http://smarterer.com/tests/facebook/run?wuid=ogiwmjfhmm" class="smarterer_test_link"><img src="http://smarterer.com/test-widget/adu/rect/facebook.png" alt="Facebook" /></a>
+<h1>You must configure a widget embed</h1>
 '''
 
 # smarterer test id to display a leaderboard for
 TEST_ID = 2
 
 
-SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+SECRET_KEY = 'SOOPERSEKRET'
