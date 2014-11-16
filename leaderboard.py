e1e8473b8ccb2a89327bc122cd06e5ac291ad655
Anand Thakker <vestibule@anandthakker.net>
2014-11-16 20:01:26 +0100

    Rearrange and document code.

--- leaderboard.py
+++ leaderboard.py
@@ -9,68 +9,87 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    '''
+    Front page.
+    
+    Either a welcome landing page with a login link or redirect to user's home.
+    '''
+    
     if 'username' in session:
-        name = escape(session['username'])
-        conn = sqlite3.connect('example_leaderboard.db')
-        c = conn.cursor()
-        c.execute("SELECT access_token FROM user_tokens WHERE username=?",
-                                                                    (name,))
-        t = c.fetchone()
-        conn.close()
+        return redirect(url_for('home', username=session['username']))
     else:
-        name = None
-        t = None
-    api = smarterer.api.Smarterer(client_id=config.SMARTERER_CLIENT_ID)
-    oauth_url = api.authorize_url(callback="localhost:5000/")
-    return render_template('welcome.html', username=name,
-                                        oauth_url=oauth_url,
-                                        authd=bool(t),
-                                        widget_embed=config.SMARTERER_WIDGET_EMBED)
+        api = smarterer.api.Smarterer(client_id=config.SMARTERER_CLIENT_ID)
+        oauth_url = api.authorize_url()
+        return render_template('welcome.html', username=None,
+                                            oauth_url=oauth_url,
+                                            authd=False,
+                                            widget_embed=config.SMARTERER_WIDGET_EMBED)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    '''
+    Login: show or handle the login form.
+    '''
+    if request.method != 'POST':
+        # just render the login form for a GET request.
+        return render_template('login.html')
+    else:
+        # handle the login: for this example just naively save the username
+        # in the session and then redirect.
         session['username'] = request.form['username']
         return redirect(url_for('index'))
-    return render_template('login.html')
-    # return '''
-    #     <form action="" method="post">
-    #         <p>Type a username to log in: <input type="text" name="username"></p>
-    #         <p><input type=submit value=Login></p>
-    #     </form>
-    # '''
 
 
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it's there
+    '''
+    Logout: remove the username from the session if it's there
+    '''
     session.pop('username', None)
     return redirect(url_for('index'))
 
 
-@app.route('/leaderboard')
-def leaderboard():
-    if 'username' in session:
-        username = escape(session['username'])
-        util.update_badges(username)
-    else:
-        username = None
+@app.route('/user/<username>')
+def home(username):
+    '''
+    Logged in user's home page.
+    
+    Check if we've saved a Smarterer access token saved for the user.
+    If not, provide a link for them to authorize with smarterer. If so, 
+    show the embedded test widget. (See templates/home.html.)
+    '''
+    name = escape(username)
 
     conn = sqlite3.connect('example_leaderboard.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM user_badges WHERE test_url_slug=?", (config.TEST_URL_SLUG,))
-    d = [{'username': badge_username, 
-        'test_url_slug': test_url_slug,
-        'raw_score': raw_score,
-        'badge_image': badge_image} for (badge_username, test_url_slug, raw_score, badge_image) in c.fetchall()]
+    c.execute("SELECT access_token FROM user_tokens WHERE username=?",
+                                                                (name,))
+    is_authorized = bool(c.fetchone())
     conn.close()
-    badges = sorted(d, key=lambda badge: badge['raw_score'], reverse=True)
-    return render_template('leaderboard.html', badges=badges, username=username)
+    
+    # Build the OAuth URL for the Authorize with Smarterer Link.
+    api = smarterer.api.Smarterer(client_id=config.SMARTERER_CLIENT_ID)
+    oauth_url = api.authorize_url()
+
+    return render_template('home.html', username=name,
+                                        oauth_url=oauth_url,
+                                        is_authorized=is_authorized,
+                                        widget_embed=config.SMARTERER_WIDGET_EMBED)
 
 
 @app.route('/smarterer_auth_complete')
 def auth_complete():
+    '''
+    This is the OAuth callback handler.  After we send the user to Smarterer
+    and s/he authorizes access, Smarterer redirects her/him back to this URL
+    with a temporary `code` query parameter, which we use to get the longer-
+    term access token for this user that we'll save in the db so that our app
+    can continue to access the user's Smarterer data.
+        
+    (Recall that we configured this particular URL when we registered the
+    Leaderboard app with Smarterer.)
+    '''
     username = session.get('username', None)
     if not username:
         return redirect(url_for('login'))
@@ -88,6 +107,40 @@ def auth_complete():
     conn.commit()
     conn.close()
     return redirect(url_for('index'))
+    
+
+@app.route('/leaderboard')
+def leaderboard():
+    '''
+    The leaderboard.
+
+    If there's a logged in user, first ensure that her/his score has been saved 
+    (or updated) in the database.
+
+    Then pull the stored user test scores from the db and sort them.
+    '''
+
+    # Check for logged in user and insert/refresh score in db if so.
+    if 'username' in session:
+        username = escape(session['username'])
+        util.update_badges(username)
+    else:
+        username = None
+
+    # Pull scores from db and render the sorted list.
+    conn = sqlite3.connect('example_leaderboard.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM user_badges WHERE test_url_slug=?", (config.TEST_URL_SLUG,))
+    d = [{'username': badge_username, 
+        'test_url_slug': test_url_slug,
+        'raw_score': raw_score,
+        'badge_image': badge_image} for (badge_username, test_url_slug, raw_score, badge_image) in c.fetchall()]
+    conn.close()
+    badges = sorted(d, key=lambda badge: badge['raw_score'], reverse=True)
+    return render_template('leaderboard.html', badges=badges, username=username)
+
+
+
 
 # set the secret key.  keep this really secret:
 app.secret_key = config.SECRET_KEY
@@ -95,5 +148,3 @@ app.secret_key = config.SECRET_KEY
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
--- smarterer/api.py
+++ smarterer/api.py
@@ -1,5 +1,7 @@
 """
-Simple Python wrapper for the Smarterer API.
+Simple Python wrapper for the Smarterer REST API.
+
+See the docs at http://smarterer.com/developers/apidoc.
 """
 
 import requests
@@ -25,12 +27,65 @@ class Smarterer(object):
         self.client_secret = client_secret
         self.verify = verify
 
+    ############################################################################
+    # Authentication
+    ############################################################################
+    
+    def authorize_url(self):
+        '''
+        Build the OAuth authorization URL.  This is where our webapp will send
+        the user to authenticate/authorize via Smarterer.
+        '''
+        return '{oauth}/authorize?client_id={client_id}'.format(
+                                                    oauth=OAUTH_BASE_URL,
+                                                    client_id=self.client_id)
+
+    def get_access_token(self, code=None):
+        '''
+        Using the code provided by Smarterer as a query parameter on the 
+        callback URL, request an access token from the access_token endpoint.
+        '''
+        token_url = '{oauth}/access_token'.format(oauth=OAUTH_BASE_URL)
+        resp = requests.get(token_url, params=dict(client_id=self.client_id,
+                                       client_secret=self.client_secret,
+                                       code=code,
+                                       grant_type='authorization_code'),
+                                       verify=self.verify)
+        if resp.status_code != 200:
+            raise SmartererApiHttpException("Not-OK response. HTTP Status=%s, "
+                                            "Response=%r" % (
+                                                resp.status_code,
+                                                resp.content))
+        return str(json.loads(resp.content)['access_token'])
+
+
+    ############################################################################
+    # REST API
+    ############################################################################
+
+    def badges(self):
+        '''
+        Request the badges for the user associated with self.access_token.
+        '''
+        return self._req("badges")
+
+    def _req(self, resource_name):
+        """
+        Make an API request over HTTP and attempt to parse the JSON result.
+
+        :param resource_name: Path to the API resource under the main
+                              API_BASE_URL.
+        """
+        # build the service URL
+        url = API_BASE_URL + resource_name
+        return json.loads(self._fetch(url))
+        
     def _fetch(self, url):
         """
-        Fetch a URL. If anything other than a 200 OK response is received,
-        raise an exception. Otherwise, return the content.
+        Fetch a data from a REST endpoint URL. If anything other than a 200 OK 
+        response is received, raise an exception. Otherwise, return the content.
         """
-        # create a dictionary of parameters that are available
+        # create a dictionary of request parameters that are available
         params = dict(filter(lambda x: x[1] is not None,
                                     (('client_id', self.client_id),
                                      ('access_token', self.access_token))
@@ -47,41 +102,3 @@ class Smarterer(object):
                                                 response.content))
         # otherwise, return the content
         return response.content
-
-    def _req(self, resource_name):
-        """
-        Make an API request over HTTP and attempt to parse the JSON result.
-
-        :param resource_name: Path to the API resource under the main
-                              API_BASE_URL.
-        """
-        # build the service URL
-        url = API_BASE_URL + resource_name
-        return json.loads(self._fetch(url))
-
-    def badges(self, username=None):
-        resource = "badges"
-        if username:
-            resource = "/".join((resource, username))
-        return self._req(resource)
-
-    def authorize_url(self, callback=''):
-        return '{oauth}/authorize?client_id={client_id}'.format(
-                                                    oauth=OAUTH_BASE_URL,
-                                                    client_id=self.client_id)
-
-    def get_access_token(self, code=None):
-        token_url = '{oauth}/access_token'.format(oauth=OAUTH_BASE_URL)
-        resp = requests.get(token_url, params=dict(client_id=self.client_id,
-                                       client_secret=self.client_secret,
-                                       code=code,
-                                       grant_type='authorization_code'),
-                                       verify=self.verify)
-        if resp.status_code != 200:
-            raise SmartererApiHttpException("Not-OK response. HTTP Status=%s, "
-                                            "Response=%r" % (
-                                                resp.status_code,
-                                                resp.content))
-        return str(json.loads(resp.content)['access_token'])
-
-
